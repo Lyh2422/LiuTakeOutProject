59950e0c22f1da345475a9c56ecbe036a335bd62
刘远浩 <1951381654@qq.com>
2024-06-10 00:40:02 +0800

    工作台相关接口

--- sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
@@ -5,6 +5,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface SetmealMapper {
@@ -26,4 +27,11 @@ public interface SetmealMapper {
 
     @Select("select * from setmeal where id=#{id}")
     Setmeal getById(Long setmealId);
+
+    /**
+     * 根据条件统计套餐数量
+     * @param map
+     * @return
+     */
+    Integer countByMap(Map map);
 }
